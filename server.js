57aab4453c83ce0ae09c8ceee166325b7bd14ce0
A. Joseph <nwodojoseph88@gmail.com>
2025-02-03 20:13:45 +0100

    modified app.listen console statement

--- server.js
+++ server.js
@@ -2093,5 +2093,5 @@ async function getFormattedTitle(title) {
 const PORT = process.env.PORT;
 const HOST = process.env.HOST;
 app.listen(PORT, HOST, () => {
-  console.log(`Server is running on http://${HOST}/${PORT}`);
+  console.log(`Server is running on http://${HOST}:${PORT}`);
 });
